d27782f61ebb9df41167819772a7654e903a815c
Tomivan <gracejunkie20@gmail.com>
2022-01-18 03:26:37 +0100

    project setup and api call

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card" v-for="(item, index) in photoUrls" :key="{index}">
+        <img :src="item.small" alt="" class="photo">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+   data() {
+    return {
+      photoUrls: [],
+      names: []
+    }
+    },
+    created() {
+      this.getUnsplashPhotos()
+      this.searchPhotos()
+  },
+   methods: {
+    getUnsplashPhotos() {
+     const headers = { "Authorization": "Client-ID jF5H3GQKQhaKdSecL9UestPChaSxMAaUaUps5oXopbc"};
+  fetch("https://api.unsplash.com/photos",  { headers })
+    .then(response => response.json())
+    .then(data => {
+        console.log('data: ', data)
+        const photos = data;
+  if (photos) {
+        this.photoUrls = photos.map(p => p.urls);
+      }
+    })
+    .catch(error => console.log('error: ', error))
+  }
+  },
+  searchPhotos() {
+     const headers = { "Authorization": "Client-ID jF5H3GQKQhaKdSecL9UestPChaSxMAaUaUps5oXopbc"};
+     fetch("https://api.unsplash.com/search/photos",  { headers })
+    .then(response => response.json())
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+  margin: 0 2% 2% 0;
+}
+.photo{
+  width: 14vw;
+  height: 30vh;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/SearchPhoto.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+     <div class="input">
+        <i class="fa fa-search search-icon" aria-hidden="true"></i>
+        <input type="text" placeholder="Search for photo" v-model="search">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchPhoto'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input{
+  background: #c8c8c8;
+  padding: 2em;
+}
+input{
+  height: 7vh;
+  width: 65vw;
+  border: 1px solid #0000001e;
+  border-radius: 5px;
+  margin-left: 15%;
+}
+input::placeholder{
+  padding-left: 1em;
+}
+</style>
